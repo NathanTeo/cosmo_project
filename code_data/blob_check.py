7cf8f1ecae14b09777a1589aacf31b2c329e1e8d
NathanTeo <teonathan11@gmail.com>
2024-06-21 15:08:46 -0700

    Bug fixes
    change folder paths to work with linux

--- code_data/blob_check.py
+++ code_data/blob_check.py
@@ -6,17 +6,17 @@ Author: Nathan Teo
 import numpy as np
 import matplotlib.pyplot as plt
 
-blob_num = 10
+blob_num = 1
 generation_seed = 70
 sample_num = 10000
 image_size = 28
 blob_size = 5
 
-root_path = "C:\\Users\\Idiot\\Desktop\\Research\\OFYP\\cosmo_project"
-data_dir=f'{root_path}\\Data\\{blob_num}_blob'
+root_path = "C:/Users/Idiot/Desktop/Research/OFYP/cosmo_project"
+data_dir=f'{root_path}/Data/{blob_num}_blob'
 file_name = f'{blob_num}blob_imgsize{image_size}_blobsize{blob_size}_samplenum{sample_num}_seed{generation_seed}'
 
-samples = np.load(f'{data_dir}\\{file_name}.npy')
+samples = np.load(f'{data_dir}/{file_name}.npy')
     
 for i, sample in enumerate(samples):
     if i == 0:
