a44cbc196a395f5d7420dd3943fc0bec4607b870
NathanTeo <teonathan11@gmail.com>
2024-08-07 11:05:34 -0700

    change import data to work without root folder

--- utils/import_data.py
+++ utils/import_data.py
@@ -3,9 +3,8 @@ import os
 import platform
 
 run = 'bug_finder'
-root_path = "/Users/Idiot/Desktop/Research/OFYP/cosmo"
 
-sys.path.append(f'{root_path}/cosmo_runs/{run}')
+sys.path.append(f'./cosmo_runs/{run}')
 
 from config.model_params import generation_params
 
@@ -21,12 +20,15 @@ data_source_path = f'cosmo_data/{blob_num}_blob'
 data_dest_path = f'cosmo_runs/{run}'
 data_file_name = f'bn{blob_num}{data_distribution[0]}-is{image_size}-bs{blob_size}-sn{sample_num}-sd{generation_seed}-ns{int(gen_noise)}.npy'
 
-source = f'{root_path}/{data_source_path}'
-dest = f'{root_path}/{data_dest_path}/data'
+source = f'./{data_source_path}'
+dest = f'./{data_dest_path}/data'
 
 if not os.path.exists(dest):
         os.makedirs(dest)
 
-os.system(f'robocopy {source} {dest} {data_file_name} /e')
+if platform.system()=='Windows':
+        os.system(f'robocopy {source} {dest} {data_file_name} /e')
+elif platform.system()=='Linux':
+        os.system(f'cp {source}/{data_file_name} {dest}')
 
 print('data moved')
