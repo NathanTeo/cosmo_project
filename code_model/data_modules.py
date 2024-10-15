3fb83887f98605d13248458c1a8c4f00a9072388
Nathan Teo <teonathan11@gmail.com>
2024-10-15 13:16:59 -0700

    fix wrong variable name, self.samples to samples

--- code_model/data_modules.py
+++ code_model/data_modules.py
@@ -28,7 +28,7 @@ class BlobDataModule(pl.LightningDataModule):
         # Very bad way to handle, is there a way to get this without loading data?
         samples = np.load(data_file)
         self.num_samples = len(samples)
-        self.scaling_factor = 1/np.max([np.max(self.samples), np.abs(np.min(self.samples))])
+        self.scaling_factor = 1/np.max([np.max(samples), np.abs(np.min(samples))])
         print(f'Data module scaling factor: {self.scaling_factor}')
         
         self.truncate_ratio = truncate_ratio
